e60af0a8c606d9a4ae2247a845280b29f08397a3
kifrud <tfgdurf@gmail.com>
2024-06-14 18:33:54 +0300

    feat: Toast

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,10 +1,12 @@
-import { Component, JSX, splitProps } from 'solid-js'
+import { Component, JSX, Show, createEffect, createMemo, splitProps, untrack } from 'solid-js'
 import { ToastRootProps, Toast as ToastUi } from '@kobalte/core/toast'
 import type { ToastPayload } from '@/helpers'
+import { Dictionary, useAppContext } from '@/contexts'
 import CloseIcon from '~icons/material-symbols/close-rounded'
 import './styles.scss'
 
 interface ToastProps extends ToastRootProps {
+  translate?: boolean
   payload: ToastPayload | JSX.Element
   state?: 'pending' | 'fulfilled' | 'rejected'
   error?: unknown
@@ -13,7 +15,15 @@ interface ToastProps extends ToastRootProps {
 }
 
 const Toast: Component<ToastProps> = props => {
-  const [unique, rest] = splitProps(props, ['data', 'state', 'error', 'payload', 'class'])
+  const { t } = useAppContext()
+  const [unique, rest] = splitProps(props, [
+    'data',
+    'state',
+    'error',
+    'payload',
+    'class',
+    'translate',
+  ])
 
   if (unique.payload instanceof Element) {
     return <ToastUi {...rest}>{unique.payload as Element}</ToastUi>
@@ -21,25 +31,32 @@ const Toast: Component<ToastProps> = props => {
 
   const payload = () => unique.payload as ToastPayload
 
+  const Icon = payload().icon
+
+  const title = createMemo(() =>
+    unique.translate ? (t(payload().title as keyof Dictionary) as JSX.Element) : payload().title
+  )
+
+  const msg = createMemo(() =>
+    unique.translate ? (t(payload().message as keyof Dictionary) as JSX.Element) : payload().message
+  )
 
   return (
     <ToastUi class={`toast ${unique.class}`} {...rest}>
-      <div class="toast__content-wrp">
+      <div class="toast__body">
+        {Icon && (
+          <div class="toast__icon-wrp icon-24">
+            <Icon />
+          </div>
+        )}
         <div class="toast__content">
-          <div class="toast__icon-wrp">
-            {payload().icon}
-          </div>
-          <div>
-            <ToastUi.Title class="toast__title">{payload().title}</ToastUi.Title>
-            <ToastUi.Description class="toast__description">
-              {payload().message}
-            </ToastUi.Description>
-          </div>
+          <ToastUi.Title class="toast__title">{title()}</ToastUi.Title>
+          <ToastUi.Description class="toast__description">{msg()}</ToastUi.Description>
         </div>
-        <ToastUi.CloseButton class="toast__close-button">
-          <CloseIcon />
-        </ToastUi.CloseButton>
       </div>
+      <ToastUi.CloseButton class="toast__close-button">
+        <CloseIcon />
+      </ToastUi.CloseButton>
       <ToastUi.ProgressTrack class="toast__progress-track">
         <ToastUi.ProgressFill class="toast__progress-fill" />
       </ToastUi.ProgressTrack>
--- src/helpers/notification.tsx
+++ src/helpers/notification.tsx
@@ -1,7 +1,6 @@
 import { toaster } from '@kobalte/core/toast'
 import { Toast } from '@/components'
-import { JSX, createMemo } from 'solid-js'
-import { useAppContext } from '@/contexts'
+import { Component, JSX, createMemo } from 'solid-js'
 import SuccessIcon from '~icons/material-symbols/check-circle-outline'
 import ErrorIcon from '~icons/material-symbols/error-outline'
 import InfoIcon from '~icons/material-symbols/info-outline'
@@ -16,88 +15,100 @@ interface BaseToastPayload {
 export type ToastPayload = BaseToastPayload &
   Partial<{
     title: string
-    icon: JSX.Element
+    icon: Component<JSX.HTMLAttributes<HTMLOrSVGElement>>
   }>
 
+const defaultTitles: Record<ToastType, string> = {
+  success: 'global.notification.title.success',
+  error: 'global.notification.title.error',
+  info: 'global.notification.title.info',
+  warning: 'global.notification.title.warning',
+}
+
+const defaultMessages: Record<ToastType, string> = {
+  success: 'global.notification.message.success',
+  error: 'global.notification.message.error',
+  info: 'global.notification.message.info',
+  warning: 'global.notification.message.warning',
+}
+
+const defaultIcons: Record<ToastType, Component> = {
+  success: SuccessIcon,
+  error: ErrorIcon,
+  info: InfoIcon,
+  warning: WarningIcon,
+}
+
+const generateContent = (type: ToastType, payload?: ToastPayload | JSX.Element) => {
+  if (payload && (typeof payload !== 'object' || !('message' in payload!))) {
+    return payload as JSX.Element
+  } else {
+    const msgPayload = payload as ToastPayload | undefined
+
+    const title = msgPayload?.title ?? defaultTitles[type]
+    const message = msgPayload?.message ?? defaultMessages[type]
+    const icon = msgPayload?.icon ?? defaultIcons[type]
+
+    return {
+      title,
+      message,
+      icon,
+    } satisfies ToastPayload
+  }
+}
+
+/**
+ * Function to call notification toasts
+ *
+ * @param type Type of the toast (default: `'info'`)
+ * @param payload Toast content
+ * @param promise If toast should handle promise
+ * @param translate If toast should translate `message` and `title` fields in `payload` arg (default: `true`)
+ * @returns Toast's `id`
+ */
 export function useNotification<T extends unknown>(
   type: ToastType = 'info',
   payload?: ToastPayload | JSX.Element,
-  promise?: Promise<T> | (() => Promise<T>)
+  promise?: Promise<T> | (() => Promise<T>),
+  translate: boolean = true
 ) {
-  const { t } = useAppContext()
+  const content = generateContent(type, payload)
 
-  const defaultTitles: Record<ToastType, string> = {
-    success: t('global.notification.title.success'),
-    error: t('global.notification.title.error'),
-    info: t('global.notification.title.info'),
-    warning: t('global.notification.title.warning'),
-  }
-
-  const defaultMessages: Record<ToastType, string> = {
-    success: t('global.notification.message.success'),
-    error: t('global.notification.message.error'),
-    info: t('global.notification.message.info'),
-    warning: t('global.notification.message.warning'),
-  }
-
-  const defaultIcons: Record<ToastType, JSX.Element> = {
-    success: <SuccessIcon />,
-    error: <ErrorIcon />,
-    info: <InfoIcon />,
-    warning: <WarningIcon />,
-  }
-
-  const generateContent = (type: ToastType, payload?: ToastPayload | JSX.Element) => {
-    if (typeof payload !== 'object' || !('message' in payload!)) {
-      return payload as JSX.Element
-    } else {
-      const title = payload.title ?? defaultTitles[type]
-      const message = payload.message ?? defaultMessages[type]
-      const icon = payload.icon ?? defaultIcons[type]
-
-      return {
-        title,
-        message,
-        icon,
-      } satisfies ToastPayload
-    }
-  }
-
-  const content = createMemo(() => generateContent(type, payload))
-
-  let id: number
-
-  if (promise) {
-    id = toaster.promise(promise, props => (
+  if (!promise) {
+    return toaster.show(props => (
+      <Toast
+        class={`toast--${type}`}
+        toastId={props.toastId}
+        payload={content}
+        translate={translate}
+      />
+    ))
+  } else {
+    return toaster.promise(promise, props => (
       <Toast
         class={`toast--${type}`}
         toastId={props.toastId}
         state={props.state}
-        payload={content()}
+        payload={content}
         error={props.error}
         data={props.data}
+        translate={translate}
       />
     ))
-  } else {
-    id = toaster.show(props => (
-      <Toast class={`toast--${type}`} toastId={props.toastId} payload={content()} />
-    ))
   }
+}
 
-  const updateToast = (type: ToastType = 'info', payload?: ToastPayload | JSX.Element) => {
-    const content = createMemo(() => generateContent(type, payload))
+export const clearToasts = () => toaster.clear()
+export const removeToast = (id: number) => toaster.dismiss(id)
 
-    return toaster.update(id, props => (
-      <Toast class={`toast--${type}`} toastId={props.toastId} payload={content()} />
-    ))
-  }
+export const updateToast = (
+  id: number,
+  type: ToastType = 'info',
+  payload?: ToastPayload | JSX.Element
+) => {
+  const content = createMemo(() => generateContent(type, payload))
 
-  const removeToast = () => toaster.dismiss(id)
-  const clearToasts = () => toaster.clear()
-
-  return {
-    updateToast,
-    removeToast,
-    clearToasts,
-  }
+  return toaster.update(id, props => (
+    <Toast class={`toast--${type}`} toastId={props.toastId} payload={content()} />
+  ))
 }
