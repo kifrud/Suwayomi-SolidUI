e785b196521f09ea76e7e70b69944fed9483e19e
kifrud <tfgdurf@gmail.com>
2024-06-18 11:46:55 +0300

    refactor: UpdateCheck now in @/components

--- src/pages/Updates/index.tsx
+++ src/pages/Updates/index.tsx
@@ -1,6 +1,29 @@
-import { Component } from 'solid-js'
+import { useGraphQLClient, useHeaderContext } from '@/contexts'
+import { updates } from '@/gql/Queries'
+import { createQuery } from '@tanstack/solid-query'
+import { Component, createSignal, onCleanup, onMount } from 'solid-js'
 
-const Updates: Component<{}> = () => {
+const Updates: Component = () => {
+  const headerCtx = useHeaderContext()
+  const client = useGraphQLClient()
+
+  const [offset, setOffset] = createSignal(0)
+
+  const update = createQuery(() => ({
+    queryKey: ['updates', offset()],
+    queryFn: async () => {
+      return await client.query(updates, { offset: offset() }).toPromise()
+    },
+  }))
+
+  onMount(() => {
+    headerCtx.setHeaderEnd(null)
+  })
+
+  onCleanup(() => {
+    headerCtx.clear()
+  })
+
   return <div></div>
 }
 
