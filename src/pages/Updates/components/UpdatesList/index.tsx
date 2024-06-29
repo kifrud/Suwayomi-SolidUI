dc3efb7d19def5d59a54ffa82e56085e2dcc8fe9
kifrud <tfgdurf@gmail.com>
2024-06-29 22:47:05 +0300

    fix: add updatelist

---
+++ src/pages/Updates/components/UpdatesList/index.tsx
@@ -0,0 +1,46 @@
+import { Component, For } from 'solid-js'
+import { SetStoreFunction } from 'solid-js/store'
+import { createQuery } from '@tanstack/solid-query'
+import { useGraphQLClient } from '@/contexts'
+import { getDownloadStatus } from '@/gql/Queries'
+import { UpdateNode } from '../..'
+import UpdateItem from './UpdateItem'
+
+interface UpdatesListProps {
+  updates: Record<string, UpdateNode[]>
+}
+
+const UpdatesList: Component<UpdatesListProps> = props => {
+  const client = useGraphQLClient()
+
+  const downloadStatus = createQuery(() => ({
+    queryKey: ['donwloadStatus'],
+    queryFn: async () => client.query(getDownloadStatus, {}).toPromise(),
+  }))
+
+  return (
+    <For each={Object.entries(props.updates)}>
+      {([date, items]) => (
+        <>
+          <span class="updates__group-date">{date}</span>
+          <For each={items}>
+            {item => (
+              <UpdateItem
+                item={item}
+                isDownloaded={
+                  !!downloadStatus.data?.data?.downloadStatus.queue.find(
+                    q =>
+                      item.sourceOrder === q.chapter.sourceOrder &&
+                      item.manga.id === q.chapter.manga.id
+                  )
+                }
+              />
+            )}
+          </For>
+        </>
+      )}
+    </For>
+  )
+}
+
+export default UpdatesList
