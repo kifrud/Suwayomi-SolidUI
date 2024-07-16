b54c72ac40f58f6eb7413f53d34231057adb7fe1
kifrud <tfgdurf@gmail.com>
2024-07-16 16:02:28 +0300

    feat: hover helper, some refactor & enhancements WIP

--- src/pages/manga/components/ChapterList/index.tsx
+++ src/pages/manga/components/ChapterList/index.tsx
@@ -1,9 +1,8 @@
 import { Component, For, createMemo } from 'solid-js'
-import { A } from '@solidjs/router'
 import { MangaMeta } from '@/contexts'
-import { ChapterSort, RoutePaths } from '@/enums'
 import { TManga } from '@/types'
 import { filterChapters, sortChapters } from '@/helpers'
+import ChapterListItem from './ChapterListItem'
 
 interface ChapterListProps {
   manga: TManga | undefined
@@ -24,25 +23,7 @@ const ChapterList: Component<ChapterListProps> = props => {
       <span class="font-bold">{props.manga?.manga.chapters.totalCount}</span>
       <div class="flex flex-col gap-2">
         <For each={sortedChapters()}>
-          {chapter => (
-            <A
-              href={`${RoutePaths.manga}/${props.manga?.manga.id}${RoutePaths.chapter}/${chapter.id}`}
-              class="flex justify-between transition-all p-1"
-              classList={{
-                'opacity-50 hover:opacity-80': chapter.isRead,
-                'opacity-100 hover:opacity-80': !chapter.isRead,
-              }}
-            >
-              <div class="flex flex-col">
-                <span>{chapter.name}</span>
-                <span>{chapter.scanlator}</span>
-                <span>#{chapter.sourceOrder}</span>
-              </div>
-              <div>
-                <span>{new Date(+chapter.uploadDate).toLocaleDateString()}</span>
-              </div>
-            </A>
-          )}
+          {chapter => <ChapterListItem chapter={chapter} manga={props.manga} />}
         </For>
       </div>
     </div>
