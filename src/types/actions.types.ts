55778bf00f97ef515071c1f40d57a11e85135ed8
kifrud <tfgdurf@gmail.com>
2024-07-29 11:43:44 +0300

    feat: chaptersSelection

--- src/types/actions.types.ts
+++ src/types/actions.types.ts
@@ -1,3 +1,8 @@
 export type BaseActions = 'download' | 'markAsRead' | 'markAsUnread' | 'delete'
 export type LibraryActions = BaseActions | 'editCategory'
 export type ChapterActions = BaseActions | 'bookmark' | 'markAsReadBefore' | 'read'
+export type SelectionChapterActions =
+  | BaseActions
+  | 'addBookmark'
+  | 'removeBookmark'
+  | 'markAsReadBefore'
